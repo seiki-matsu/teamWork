2c52360d372293e41569a52962c130d91bb3116a
seiki <seiki.forgit@gmail.com>
2024-01-16 14:24:31 +0900

    Membersファイルにtestフィールドを追加
    String型でのtestフィールドを追加することで対応できるようにした。

--- Members.java
+++ Members.java
@@ -9,6 +9,7 @@ public class Members {
 	private String email;
 	private String phoneNumber;
 	private java.sql.Date birthday;
+	private String test;
 
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
 
